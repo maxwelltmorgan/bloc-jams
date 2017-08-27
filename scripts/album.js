9d99f00e747631e9facbd1d0e3076b38fcbcb4cc
Maxwell Morgan <maxwelltmorgan@gmail.com>
2017-08-27 02:05:28 -0600

    Assignment 19 completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,12 @@
+ var setSong = function(songNumber) {
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+ };
+
+ var getSongNumberCell = function(number) {
+    return $('.song-item-number[data-song-number="' + number + '"]');
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -20,8 +29,7 @@
 	        if (currentlyPlayingSongNumber !== songNumber) {
 		          // Switch from Play -> Pause button to indicate new song is playing.
 		          $(this).html(pauseButtonTemplate);
-              currentlyPlayingSongNumber = songNumber;
-              currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+              setSong(songNumber);
               updatePlayerBarSong();
 	        } else if (currentlyPlayingSongNumber === songNumber) {
 		          // Switch from Pause -> Play button to pause currently playing song.
